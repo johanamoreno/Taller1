bb8e7a469c6e733d31a4710ce055d059ee16f5b7
johanamoreno <71187100+johanamoreno@users.noreply.github.com>
2021-08-21 17:40:38 -0500

    Initial commit

---
+++ funcion2.js
@@ -0,0 +1,14 @@
+/*let codigoNave="ARQ2555:Sara Bel-Sun";
+
+let cajones=codigoNave.split(":");
+
+console.log(cajones [1]);*/
+
+function consultarNombre(codigoNave){
+
+    return (codigoNave.split(":"));
+}
+
+console.log(`El nombre del conductor de la nave ARQ2555 es: ${consultarNombre("ARQ2555:Sara Bel-Sun")[1]}`);
+console.log(`El nombre del conductor de la nave ARQ2556 es: ${consultarNombre("ARQ2556:Nodin Chavdri")[1]}`);
+console.log(`El nombre del conductor de la nave ARQ2557 es: ${consultarNombre("ARQ2557:Fin")[1]}`);
